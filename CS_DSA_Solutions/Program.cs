a1c22ce8603a432d2cfd1d34b7c7169a8652abd9
Aniket19c <aniketchaudhary19j@gmail.com>
2025-03-03 11:36:26 +0530

    Singly Linked List implemented

--- CS_DSA_Solutions/Program.cs
+++ CS_DSA_Solutions/Program.cs
@@ -8,12 +8,15 @@ namespace CS_DSA_Solutions
         {
             DoublyLinkedList dll = new DoublyLinkedList();
             CircularLinkedList cll = new CircularLinkedList();
+            SinglyLinkedList sll= new SinglyLinkedList();
+
 
             while (true)
             {
                 
                 Console.WriteLine("1. Doubly Linked List");
                 Console.WriteLine("2. Circular Linked List");
+                Console.WriteLine("3. Singly Linked List");
                 Console.WriteLine("0. Exit");
                
 
@@ -32,6 +35,10 @@ namespace CS_DSA_Solutions
                     case 2:
                         CircularLinkedListMenu(cll);
                         break;
+                    case 3:
+                        SinglyLinkedListMenu(sll);
+                        break;
+                        
                     case 0:
                         Console.WriteLine("Exiting program...");
                         return;
@@ -97,6 +104,73 @@ namespace CS_DSA_Solutions
                         int rInd;
                         if (int.TryParse(Console.ReadLine(), out rInd))
                             dll.RemoveAt(rInd);
+                        else
+                            Console.WriteLine("Invalid Input");
+                        break;
+                    case 0:
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Try again.");
+                        break;
+                }
+            }
+        }
+
+        static void SinglyLinkedListMenu(SinglyLinkedList sll)
+        {
+            while (true)
+            {
+
+                Console.WriteLine("1. Insert at End");
+                Console.WriteLine("2. Delete a Value");
+                Console.WriteLine("3. Display List");
+                Console.WriteLine("4. Add at particular index");
+                Console.WriteLine("5. Remove from index");
+                Console.WriteLine("0. Back ");
+
+
+                int choice;
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Invalid input. Try again.");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter value to insert: ");
+                        int insertVal;
+                        if (int.TryParse(Console.ReadLine(), out insertVal))
+                            sll.InsertEnd(insertVal);
+                        else
+                            Console.WriteLine("Invalid input.");
+                        break;
+                    case 2:
+                        Console.Write("Enter value to delete: ");
+                        int deleteVal;
+                        if (int.TryParse(Console.ReadLine(), out deleteVal))
+                            sll.Delete(deleteVal);
+                        else
+                            Console.WriteLine("Invalid input.");
+                        break;
+                    case 3:
+                        sll.Display();
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter the index where you want to add");
+                        int ind;
+                        if (int.TryParse(Console.ReadLine(), out ind))
+                            Console.WriteLine("Enter the data you want to add");
+                        int data;
+                        if (int.TryParse(Console.ReadLine(), out data)) ;
+                        sll.InsertAt(ind, data);
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter the index you want to delete");
+                        int rInd;
+                        if (int.TryParse(Console.ReadLine(), out rInd))
+                            sll.RemoveAt(rInd);
                         else
                             Console.WriteLine("Invalid Input");
                         break;
